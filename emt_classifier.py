c1f995bf396851184677073a3e44e54c7b44e5da
tr0up2r <kmydtr@gmail.com>
2021-07-20 19:14:42 +0900

    emt_classifier 추가, classifier test size, epoch 등 수정.

--- emt_classifier.py
+++ emt_classifier.py
@@ -14,11 +14,11 @@ from transformers import AdamW, get_linear_schedule_with_warmup
 from sklearn.metrics import f1_score
 
 
-df = pd.read_csv('data/data_for_coding_inf.csv', encoding='ISO-8859-1')
-inf_count = df['inf'].value_counts()
-print(f'inf_count :\n{inf_count}\n')
+df = pd.read_csv('data/data_for_coding_emt.csv', encoding='ISO-8859-1')
+emt_count = df['emt'].value_counts()
+print(f'emt_count :\n{emt_count}\n')
 
-possible_labels = df.inf.unique()
+possible_labels = df.emt.unique()
 
 # 3, 2, 1 역순으로 되어 있는 것을 reverse 해주어 보다 직관적인 이해를 도움.
 possible_labels = possible_labels[::-1]
@@ -30,14 +30,14 @@ for index, possible_labels in enumerate(possible_labels):
     label_dict[possible_labels] = index
 print(label_dict)
 
-df['label'] = df.inf.replace(label_dict)
+df['label'] = df.emt.replace(label_dict)
 print(df, '\n')
 
 # data split (train & test sets)
 X_train, X_val, y_train, y_val = train_test_split(df.index.values,
                                                   df.label.values,
                                                   test_size=0.2,
-                                                  random_state=42,
+                                                  random_state=42,  # seed 값 설정.
                                                   stratify=df.label.values)
 
 df['data_type'] = ['not_set'] * df.shape[0]
@@ -45,9 +45,11 @@ df['data_type'] = ['not_set'] * df.shape[0]
 df.loc[X_train, 'data_type'] = 'train'
 df.loc[X_val, 'data_type'] = 'val'
 
-print(df.groupby(['inf', 'label', 'data_type']).count())
+print(df.groupby(['emt', 'label', 'data_type']).count())
 
 # BERT
+# uncased - upper case 문자들을 lower case로 바꾸고 난 후 tokenize한 모델.
+# do_lower_case - upper case를 lower case로 변환할 것인지 여부를 정하는 param.
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',
                                           do_lower_case=True)
 
@@ -103,7 +105,7 @@ optimizer = AdamW(model.parameters(),
 
 # epochs 5로 했더니 overfitting 되는 듯.
 # 3 정도가 적당?
-epochs = 5
+epochs = 10
 
 scheduler = get_linear_schedule_with_warmup(optimizer,
                                             num_warmup_steps=0,
@@ -207,7 +209,7 @@ for epoch in tqdm(range(1, epochs + 1)):
         progress_bar.set_postfix({'training_loss': '{:.3f}'.format(loss.item() / len(batch))})
         i += 1
 
-    torch.save(model.state_dict(), f'data_volume/finetuned_BERT_epoch_{epoch}_inf.model')
+    torch.save(model.state_dict(), f'data_volume/finetuned_BERT_epoch_{epoch}_emt.model')
 
     tqdm.write(f'\nEpoch {epoch}')
 
